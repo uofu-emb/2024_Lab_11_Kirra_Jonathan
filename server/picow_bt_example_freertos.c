e30e1da6125ff3c5aa55bfa104e30f8c367aad1c
KirraKotsenburg <kirrakotz@hotmail.com>
2024-11-27 12:51:21 -0700

    Added print when number of connections changes

--- server/picow_bt_example_freertos.c
+++ server/picow_bt_example_freertos.c
@@ -34,6 +34,9 @@ static void packet_handler(uint8_t packet_type, uint16_t channel, uint8_t *packe
             gap_local_bd_addr(local_addr);
             printf("BTstack up and running on %s.\n", bd_addr_to_str(local_addr));
             break;
+        case BTSTACK_EVENT_NR_CONNECTIONS_CHANGED:
+            printf("Number of connections changed to %u.\n", btstack_event_nr_connections_changed_get_number_connections(packet));
+            break;
         default:
             break;
     }
